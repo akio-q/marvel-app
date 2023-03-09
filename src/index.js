e9ccf0a621abdecd3a1582e5c0625977bd8c9cad
Valentyn <wolf.valentyn@gmail.com>
2023-03-09 16:23:50 +0200

    add MarvelService component with some methods
    The MarvelService component has getResource(), getAllCharacters(), and getCharacter() methods that work with the Marvel API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
+import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
+// const marvelService = new MarvelService();
+
+// marvelService.getAllCharacters(1011052).then(res => res.data.results.forEach(item => console.log(item.name)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
