bf08fd85a6f0f7feddde333b8c074a5a6a81345e
Valentyn <wolf.valentyn@gmail.com>
2023-06-11 14:39:10 +0300

    organize project structure into separate pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,10 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
-
-import decoration from '../../resources/img/vision.png';
+import MainPage from '../pages/MainPage';
+import ComicsPage from '../pages/ComicsPage';
 
 const App = () => {
-    const [selectedChar, setSelectedChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setSelectedChar(id);
-    }
-
     return (
         <Router>
             <div className="app">
@@ -25,22 +12,10 @@ const App = () => {
                 <main>
                     <Switch>
                         <Route exact path="/">
-                            <ErrorBoundary>
-                                <RandomChar />
-                            </ErrorBoundary>
-                            <div className="char__content">
-                                <ErrorBoundary>
-                                    <CharList onCharSelected={onCharSelected} />
-                                </ErrorBoundary>
-                                <ErrorBoundary>
-                                    <CharInfo charId={selectedChar} />
-                                </ErrorBoundary>
-                            </div>
-                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                            <MainPage />
                         </Route>
                         <Route exact path="/comics">
-                            <AppBanner />
-                            <ComicsList />
+                            <ComicsPage />
                         </Route>
                     </Switch>
                 </main>
