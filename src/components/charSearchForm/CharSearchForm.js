b9ea557866f14d9c32709d6c8f4804da0cb8ee97
Valentyn <wolf.valentyn@gmail.com>
2023-06-28 20:18:16 +0300

    refactor CharSearchForm using Formik and add Yup validation

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -1,22 +1,34 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik'; 
+import * as Yup from 'yup';
+
 import './charSearchForm.scss';
 
 const CharSearchForm = () => {
 
     return (
-        <div className="char__search-form">
-            <label htmlFor="charName" className="char__search-label">Or find a character by name:</label>
-            <div className="char__search-wrapper">
-                <input 
-                    id="charName" 
-                    name="charName" 
-                    className="char__search-input" 
-                    type="text"
-                    placeholder="Enter name" />
-                <button type="submit" className="button button__main">
-                    <div className="inner">Find</div>
-                </button>
-            </div>
-        </div>
+        <Formik
+            initialValues={{charName: ''}}
+            validationSchema={Yup.object({
+                charName: Yup.string()
+                         .min(2, 'Minimum 2 symbols')
+                         .required('Required field'), 
+            })}>
+                <Form className="char__search-form">
+                    <label htmlFor="charName" className="char__search-label">Or find a character by name:</label>
+                    <div className="char__search-wrapper">
+                        <Field 
+                            id="charName" 
+                            name="charName" 
+                            className="char__search-input" 
+                            type="text"
+                            placeholder="Enter name" />
+                        <button type="submit" className="button button__main">
+                            <div className="inner">Find</div>
+                        </button>
+                    </div>
+                    <ErrorMessage name="charName" className="char__search-error" component="div" />
+                </Form>
+        </Formik>
     );
 }
 
