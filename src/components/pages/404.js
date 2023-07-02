491ed6e582a09646e1ad560c77b90bd8c3745e1b
Valentyn <wolf.valentyn@gmail.com>
2023-07-02 18:56:36 +0300

    integrate react-helmet for dynamic meta tags and titles

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,9 +1,17 @@
-import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
+
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
 const Page404 = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="404 error" />
+                <title>404 error</title>
+            </Helmet>
             <ErrorMessage />
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '40px'}}>Oops, page doesn't exist :(</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px', 'color': '#9f0013'}} to="/">Back to main page</Link>
--- src/components/pages/singelCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singelCharacterLayout/SingleCharacterLayout.js
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({data}) => {
@@ -6,6 +8,12 @@ const SingleCharacterLayout = ({data}) => {
     return (
         <>
             <div className="single-character">
+                <Helmet>
+                    <meta
+                        name="description"
+                        content={`${name} comics book`} />
+                    <title>{`${name}'s page`}</title>
+                </Helmet>
                 <img src={thumbnail} alt='Loki' className="single-character__img" />
                 <div>
                     <h2 className="single-character__name">{name}</h2>
