3ec4e8bc436867e42bcff17649ab879af0c438b7
Valentyn <wolf.valentyn@gmail.com>
2023-07-07 15:01:12 +0300

    remove unnecessary code

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -12,16 +12,12 @@ const setContent = (process, Component, newItemsLoading) => {
     switch (process) {
         case 'waiting':
             return <Spinner />;
-            break; 
         case 'loading':
             return newItemsLoading ? <Component /> : <Spinner />;
-            break;
         case 'confirmed':
             return <Component />
-            break;
         case 'error':
             return <ErrorMessage />;
-            break;
         default:
             throw new Error('Unexpected process state');
     }
--- src/utils/setContent.js
+++ src/utils/setContent.js
@@ -5,17 +5,13 @@ import Skeleton from "../components/skeleton/Skeleton";
 const setContent = (process, Component, data) => {
     switch (process) {
         case 'waiting':
-            return <Skeleton />;
-            break; 
+            return <Skeleton />;   
         case 'loading':
             return <Spinner />;
-            break;
         case 'confirmed':
             return <Component data={data} />
-            break;
         case 'error':
             return <ErrorMessage />;
-            break;
         default:
             throw new Error('Unexpected process state');
     }
